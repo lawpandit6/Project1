4771c050c8155e074c1026742db54566d8d98696
Law Pandit <145872007+lawpandit6@users.noreply.github.com>
2024-02-02 14:55:26 +0530

    Add files via upload

---
+++ Function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int f1( int a)
+{
+
+    return a*a;
+ }
+
+int main()
+{ int b;
+   b=f1(50);
+   cout<<"square="<<b;
+    return 0;
+
+}
---
+++ loop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    char k;
+    for(i=1;i<=4;i++)
+    {
+        k='A';
+        for(j=1;j<=7;j++)
+        {
+            if(j>=i&&j<=8-i)
+            {
+            printf("%c",k);
+            j<5?k++:k--;
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
